6a2bae56c7c429a0d1f1d83a84e548835f34b086
aashna alex <aashna.alex@outlook.com>
2020-05-23 16:21:45 +0530

    underline removed

--- src/Components/NewsFeed.js
+++ src/Components/NewsFeed.js
@@ -5,7 +5,7 @@ function NewsFeed({ post }) {
     return (
         <div>
             <div style={{ display: "inline-block", marginLeft: "20px", marginTop: "50px", width: "300px", height: "500",boxShadow: "0px 0px 10px 0px rgba(0,0,0,0.75)"}}>
-            <a href={post.url} target="_blank" style = {{cursor: "pointer"}}>
+            <a href={post.url} target="_blank" style = {{cursor: "pointer", textDecoration: "none"}}>
                 <div className="Post" style={{ width: "300px", height: "500" }}>
                     <img src={post.urlToImage} style={{ width: "300px", height: "400px"}}></img>
                      <p style={{ fontFamily: "sans-serif", color: "black", marginLeft: "5px" }}>{post.title.slice(0, 50)}</p>
